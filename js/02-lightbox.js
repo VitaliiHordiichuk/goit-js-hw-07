f482c7ff7ff5a7879a0cb059059a169ca00219cb
Vialii Hordiichuk <vgordiychuk5@gmail.com>
2022-09-22 15:32:03 +0300

    hw7-part2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryTemplate(picturesList) {
+  return picturesList
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+const galleryTemplate = createGalleryTemplate(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", galleryTemplate);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
